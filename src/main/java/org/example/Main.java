370b5689482174f2e1ef21c335ea63c088e5a741
Mehmet Şükrü KAvak <mehmetsukru.kavak07@gmail.com>
2025-01-03 21:55:34 +0300

    CRUD Operations...

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,10 +11,52 @@ public class Main {
         String url = "jdbc:postgresql://localhost:5432/demo";
         String user = "postgres";
         String pass = "123456";
+
         Connection con = DriverManager.getConnection(url, user, pass);
 
         System.out.println("Connection Successful");
 
+        Statement stmt = con.createStatement();
+        read(stmt);
+
+        addData(stmt);
+
+        updateData(stmt);
+
+        delete(stmt);
+
+        read(stmt);
+
+        con.close();
+        System.out.println("Connection Closed");
     }
 
+    public static void read(Statement stmt) throws SQLException {
+
+        String query = "SELECT * FROM student order by id asc";
+        ResultSet rs = stmt.executeQuery(query);
+
+        while (rs.next()) {
+            System.out.print(rs.getInt(1) + " - ");
+            System.out.print(rs.getString(2) + " - ");
+            System.out.print(rs.getInt(3));
+            System.out.println();
+
+        }
+    }
+
+    public static void addData(Statement stmt) throws SQLException {
+        String query = "INSERT INTO student VALUES(7, 'Çakıl', 5)";
+        stmt.execute(query);
+    }
+
+    public static void updateData(Statement stmt) throws SQLException {
+        String query = "UPDATE student SET marks = 100 WHERE id = 5";
+        stmt.execute(query);
+    }
+
+    public static void delete(Statement stmt) throws SQLException {
+        String query = "DELETE FROM student WHERE id = 4";
+        stmt.execute(query);
+    }
 }
